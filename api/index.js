cf2d715e25f9b1863ff7b0cc42401414c4256c84
Deepak Kumar <deepak@poultrymitra.com>
2026-04-06 04:54:28 +0000

    Test simple API

--- api/index.js
+++ api/index.js
@@ -1,14 +1,1 @@
-import express from 'express';
-
-const app = express();
-app.use(express.json());
-
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'API working!' });
-});
-
-app.get('/health', (req, res) => {
-  res.json({ status: 'ok' });
-});
-
-export default app;
+export default (req, res) => { res.status(200).json({ test: "ok" }); };
